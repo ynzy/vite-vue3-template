5123faa08079f066bc2690f207c722dd41799e93
ynzy <758922096@qq.com>
2021-12-22 23:05:52 +0800

    feat: env添加自定义环境变量提示

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv extends Readonly<Record<string, string>> {
-  readonly VITE_ENV: string; // 环境
-  readonly VITE_OUTPUT_DIR: string; // 打包目录
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
